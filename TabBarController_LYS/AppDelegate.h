d39b61e8068c44097234a64c85e15ea1f000b059
罗 耀生 <lys1988_cool@126.com>
2012-08-23 21:43:52 +0800

    Initial Commit

---
+++ TabBarController_LYS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabBarController_LYS
+//
+//  Created by lys on 12-8-23.
+//  Copyright (c) 2012年 lys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabBarController_LYS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabBarController_LYS
+//
+//  Created by lys on 12-8-23.
+//  Copyright (c) 2012年 lys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
